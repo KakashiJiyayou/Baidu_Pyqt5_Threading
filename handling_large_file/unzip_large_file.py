fbabb56c450ecd24b95df3081edeefa2dfb81502
KakashiJiyayou <rafibapi@gmail.com>
2023-07-30 19:14:18 +0800

    handling large file issue

--- handling_large_file/unzip_large_file.py
+++ handling_large_file/unzip_large_file.py
@@ -7,11 +7,49 @@ from zipfile import ZipFile
 from bypy import ByPy
 from pyunpack import Archive
 import traceback
+import zipfile
+import locale
+
+
+
 
 root_path = os.path.dirname(__file__)
-
 path = os.path.join ( root_path, "temp" )
 
-zip_file_path = "E:\\Downloads\\wechat\\another.zip"
+shutil.rmtree ( path )
+os.mkdir(path)
+zip_file_path = r"E:\Downloads\wechat\15.柴桥一期二期检测.zip"
 
-Archive( zip_file_path ).extractall( path )
+# Archive(zip_file_path).extractall(path)
+# shutil.unpack_archive(zip_file_path, path)
+shutil.unpack_cp437_or_unicode(zip_file_path, path)
+#
+#
+# zipfile
+
+
+# temp_zip_path = zip_file_path
+# target_loc = path
+# temp_zip_path = r"" + zip_file_path
+#
+# def unpack_cp437_or_unicode(archive_path):
+#     with zipfile.ZipFile(archive_path) as zz:
+#         for zipped_name in zz.namelist():
+#             try:
+#                 print ("real name ", zipped_name)
+#                 real_name = zipped_name.decode('cp437')
+#             except UnicodeEncodeError:
+#                 real_name = zipped_name
+#
+#             with zz.open(zipped_name) as archived:
+#                 contents = archived.read()
+#             if zipped_name.endswith('/'):
+#                 dirname = os.path.join(target_loc, real_name)
+#                 if not os.path.isdir(dirname):
+#                     os.makedirs(dirname)
+#             else:
+#                 with open(os.path.join(target_loc, real_name), 'wb') as target:
+#                     target.write(contents)
+#
+#
+# unpack_cp437_or_unicode(temp_zip_path)
---
+++ handling_large_file/unzip_large_file2.py
@@ -0,0 +1,23 @@
+import zipfile, sys, os
+
+zip_file_path = ".\\temp\\软件编制 资料.zip"
+
+zf = zipfile.ZipFile(zip_file_path )
+for m in zf.infolist():
+    data = zf.read(m) # extract zipped data into memory
+    # convert unicode file path to utf8
+    disk_file_name = m.filename.encode('utf8')
+    dir_name = os.path.dirname(disk_file_name)
+    try:
+        os.makedirs(dir_name)
+    except OSError as e:
+        if e.errno == os.errno.EEXIST:
+            pass
+        else:
+            raise
+    except Exception as e:
+        raise
+    print ("disk fiel name ", disk_file_name)
+    with open(disk_file_name, 'wb') as fd:
+        fd.write(data)
+zf.close()
