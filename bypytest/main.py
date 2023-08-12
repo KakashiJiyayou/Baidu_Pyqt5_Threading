9d865787beacb0d095b946761d6767d211d0366e
KakashiJiyayou <rafibapi@gmail.com>
2023-08-12 21:40:49 +0800

    update

--- bypytest/main.py
+++ bypytest/main.py
@@ -1,3 +1,27 @@
 from bypytest.module import  database_management as Database
+from bypytest.module import runningdb as Running
+import time
 
-Database.download_db_file()
+# Running.close_connection()
+# # Database.download_db_file()
+# Database.download_db_running()
+#
+# # Running.close_connection()
+# # Running.close_connection()
+# # Running.close_connection()
+# # Running.close_connection()
+# # time.sleep(1)
+# Running.open_connection()
+# Running.open_connection()
+# Running.open_connection()
+# Running.open_connection()
+# Running.open_connection()
+# result, using_user_id = Running.is_database_in_use()
+# print ( "is database in use ", result)
+#
+# user_id = "123"  # Replace this with the actual user ID
+# result = Running.change_status( "running", user_id )
+#
+#
+# result, using_user_id = Running.is_database_in_use()
+# print ( "is database in use ", result)
